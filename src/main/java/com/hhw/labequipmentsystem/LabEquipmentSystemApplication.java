216680ad544e6fda583c1ca993c54f808fb35809
hhw114 <2730956977@qq.com>
2026-08-19 03:51:08 +0800

    第一次提交，初始化项目

---
+++ src/main/java/com/hhw/labequipmentsystem/LabEquipmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.hhw.labequipmentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabEquipmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabEquipmentSystemApplication.class, args);
+	}
+
+}
